a54b6ade26a84be7a73bce7dbd32c3f36e8b82ce
Rosen Krumov <rosenkrumov@Rosens-MacBook-Air.local>
2022-02-10 00:39:34 +0200

    add question/answer generator

---
+++ preprocess.py
@@ -0,0 +1,109 @@
+import os
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
+
+import nlp
+from transformers import T5Tokenizer, BartTokenizer, HfArgumentParser
+
+model_type = 't5'
+dataset_path = 'data/squad_multitask/'
+train_file_name = 'train_data_qg_hl_t5.pt'
+valid_file_name = 'valid_data_qg_hl_t5.pt'
+
+class DataProcessor:
+    def __init__(self, tokenizer, model_type="t5", max_source_length=512, max_target_length=32):
+        self.tokenizer = tokenizer
+        self.max_source_length = max_source_length
+        self.max_target_length = max_target_length
+        self.model_type = model_type
+        self.hl_token = "<hl>"
+        
+        if model_type == "t5":
+            self.sep_token = "<sep>"
+        elif model_type == "bart":
+            self.sep_token = "<sep>"
+        else:
+            self.sep_token = "[SEP]"
+  
+    def process(self, dataset):
+        if self.model_type == "t5":
+            dataset = dataset.map(self.add_eos_examples)
+        
+        dataset = dataset.map(self.add_special_tokens)
+        dataset = dataset.map(self.convert_to_features, batched=True)
+        
+        return dataset
+  
+    def add_eos_examples(self, example):
+        example['source_text'] = example['source_text'] + " </s>"
+        example['target_text'] = example['target_text'] + " </s>"
+        return example
+  
+    def add_special_tokens(self, example):
+        example['source_text'] = example['source_text'].replace("{hl_token}", self.hl_token)    
+        example['target_text'] = example['target_text'].replace("{sep_token}", self.sep_token)
+        return example
+  
+    def convert_to_features(self, example_batch):
+        source_encoding = self.tokenizer.batch_encode_plus(
+            example_batch['source_text'],
+            max_length=self.max_source_length,
+            padding='max_length',
+            pad_to_max_length=True,
+            truncation=True, 
+        )
+        target_encoding = self.tokenizer.batch_encode_plus(
+            example_batch['target_text'],
+            max_length=self.max_target_length,
+            padding='max_length',
+            pad_to_max_length=True,
+            truncation=True, 
+        )
+
+        encodings = {
+            'source_ids': source_encoding['input_ids'], 
+            'target_ids': target_encoding['input_ids'],
+            'attention_mask': source_encoding['attention_mask'],
+        }
+
+        return encodings
+
+if model_type == 't5':
+    tokenizer = T5Tokenizer.from_pretrained("t5-base")
+else:
+    tokenizer = BartTokenizer.from_pretrained("facebook/bart-base")
+
+tokenizer.add_tokens(['<sep>', '<hl>'])
+
+train_dataset = nlp.load_dataset(dataset_path, name='highlight_qg_format', split=nlp.Split.TRAIN)
+valid_dataset = nlp.load_dataset(dataset_path, name='highlight_qg_format', split=nlp.Split.VALIDATION)
+
+train_dataset = train_dataset.filter(lambda x: (x['task'] == 'qg'))
+valid_dataset = valid_dataset.filter(lambda x: (x['task'] == 'qg'))
+
+processor = DataProcessor(
+    tokenizer,
+    model_type=model_type,
+    max_source_length=512,
+    max_target_length=32
+)
+
+train_dataset = processor.process(train_dataset)
+valid_dataset = processor.process(valid_dataset)
+
+columns = ["source_ids", "target_ids", "attention_mask"]
+train_dataset.set_format(type='torch', columns=columns)
+valid_dataset.set_format(type='torch', columns=columns)
+
+train_path = os.path.join("data", train_file_name)
+valid_path = os.path.join("data", valid_file_name)
+
+torch.save(train_dataset, train_path)
+torch.save(valid_dataset, valid_path)
+
+tokenizer_path = f"{model_type}_qg_tokenizer"
+
+if not os.path.exists(tokenizer_path):
+    os.mkdir(tokenizer_path)
+
+tokenizer.save_pretrained(tokenizer_path)
---
+++ train.py
@@ -0,0 +1,220 @@
+import json
+import os
+import sys
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional, Union, Callable, Iterable
+
+import numpy as np
+from typing import Dict, List, Optional
+
+import torch
+from torch import nn
+
+from transformers import (
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    T5Tokenizer,
+    BartTokenizer,
+    HfArgumentParser,
+    DataCollator,
+    TrainingArguments,
+    Trainer as HFTrainer
+    set_seed,
+)
+
+from transformers.file_utils import is_apex_available
+
+from utils import freeze_embeds, assert_not_all_frozen
+
+if is_apex_available():
+    from apex import amp
+
+MODEL_TYPE_TO_TOKENIZER = {
+    "t5": T5Tokenizer,
+    "bart": BartTokenizer,
+}
+
+output_dir = 't5-small-qg-hl'
+model_type = 't5'
+model = 't5-small'
+tokenizer = 't5_qg_tokenizer'
+cache_dir = ''
+trainset_path = 'data/train_data_qg_hl_t5.pt'
+validset_path = 'data/valid_data_qg_hl_t5.pt'
+
+def trim_batch(
+    input_ids, pad_token_id, attention_mask=None,
+):
+    """Remove columns that are populated exclusively by pad_token_id"""
+    keep_column_mask = input_ids.ne(pad_token_id).any(dim=0)
+    if attention_mask is None:
+        return input_ids[:, keep_column_mask]
+    else:
+        return (input_ids[:, keep_column_mask], attention_mask[:, keep_column_mask])
+
+
+# prepares lm_labels from target_ids, returns examples with keys as expected by the forward method
+# this is necessary because the trainer directly passes this dict as arguments to the model
+# so make sure the keys match the parameter names of the forward method
+class T2TDataCollator():
+    def __init__(self, tokenizer, model_type="t5", mode='training', using_tpu=False):
+        self.tokenizer = tokenizer
+        self.model_type = model_type
+        self.mode = mode
+        self.using_tpu = False
+
+    def __call__(self, batch: List) -> Dict[str, torch.Tensor]:
+        """
+        Take a list of samples from a Dataset and collate them into a batch.
+        Returns:
+            A dictionary of tensors
+        """
+        input_ids = torch.stack([example['source_ids'] for example in batch])
+        target_ids = torch.stack([example['target_ids'] for example in batch])
+        attention_mask = torch.stack([example['attention_mask'] for example in batch])
+
+        pad_token_id = self.tokenizer.pad_token_id
+        
+        # don't trim on tpu, for some reason trimming leads to slower training on TPU
+        if not self.using_tpu:
+            input_ids, attention_mask = trim_batch(input_ids, pad_token_id, attention_mask=attention_mask)
+            target_ids = trim_batch(target_ids, pad_token_id)
+        
+        if self.model_type == "t5":
+            lm_labels = target_ids.clone()
+            decoder_input_ids = self._shift_right_t5(lm_labels)
+            if self.mode == 'training':
+                lm_labels[lm_labels[:, :] == pad_token_id] = -100
+        else:
+            decoder_input_ids = target_ids[:, :-1].contiguous()
+            lm_labels = target_ids[:, 1:].clone()
+            if self.mode == 'training':
+                lm_labels[target_ids[:, 1:] == pad_token_id] = -100
+
+        params =  {
+            "input_ids": input_ids, 
+            "attention_mask": attention_mask,
+            "labels": lm_labels,
+            "decoder_input_ids": decoder_input_ids
+        }
+        
+        return params
+    
+    def _shift_right_t5(self, input_ids):
+        decoder_start_token_id = self.tokenizer.pad_token_id
+        pad_token_id = self.tokenizer.pad_token_id
+
+        # shift inputs to the right
+        shifted_input_ids = input_ids.new_zeros(input_ids.shape)
+        shifted_input_ids[..., 1:] = input_ids[..., :-1].clone()
+        shifted_input_ids[..., 0] = decoder_start_token_id
+
+        # replace possible -100 values in labels by `pad_token_id`
+        shifted_input_ids.masked_fill_(shifted_input_ids == -100, pad_token_id)
+
+        return shifted_input_ids
+
+class Trainer(HFTrainer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    # override to support label smoothing
+    def _training_step(
+        self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]], optimizer: torch.optim.Optimizer
+    ) -> float:
+        model.train()
+        for k, v in inputs.items():
+            if isinstance(v, torch.Tensor):
+                inputs[k] = v.to(self.args.device)
+
+
+        # Our model outputs do not work with DataParallel, so forcing return tuple.
+        if isinstance(model, nn.DataParallel):
+            inputs["return_tuple"] = True
+
+        labels = inputs.pop("labels")
+        labels[labels == -100] = model.config.pad_token_id
+        outputs = model(**inputs)
+        lprobs = torch.nn.functional.log_softmax(outputs[0], dim=-1)
+        loss, nll_loss = label_smoothed_nll_loss(
+            lprobs, labels, self.label_smoothing, ignore_index=model.config.pad_token_id
+        )
+
+        if self.args.n_gpu > 1:
+            loss = loss.mean()  # mean() to average on multi-gpu parallel training
+        if self.args.gradient_accumulation_steps > 1:
+            loss = loss / self.args.gradient_accumulation_steps
+
+        if self.args.fp16:
+            with amp.scale_loss(loss, optimizer) as scaled_loss:
+                scaled_loss.backward()
+        else:
+            loss.backward()
+
+        return loss.item()
+
+def main():
+    set_seed(42)
+
+    os.environ["WANDB_PROJECT"] = "question-generation"
+
+    tokenizer_cls = MODEL_TYPE_TO_TOKENIZER[model_type]
+    tokenizer = tokenizer_cls.from_pretrained(
+        tokenizer if tokenizer else model,
+        cache_dir=cache_dir,
+    )
+    model = AutoModelForSeq2SeqLM.from_pretrained(
+        model,
+        cache_dir=cache_dir,
+    )
+
+    model.resize_token_embeddings(len(tokenizer))
+    
+    train_dataset = torch.load(train_file_path)
+    valid_dataset = torch.load(valid_file_path)
+    
+    # Initialize data_collator
+    data_collator = T2TDataCollator(
+        tokenizer=tokenizer,
+        model_type=model_type,
+        mode="training",
+        using_tpu=False
+    )
+
+    # Initialize our Trainer
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=valid_dataset,
+        data_collator=data_collator,
+        prediction_loss_only=True
+    )
+
+    # Training
+    trainer.train(
+        model_path=model
+    )
+
+    trainer.save_model()
+    
+    if trainer.is_world_master():
+        tokenizer.save_pretrained(output_dir)
+
+    # Evaluation
+    results = {}
+
+    eval_output = trainer.evaluate()
+
+    output_eval_file = os.path.join(output_dir, "eval_results.txt")
+    with open(output_eval_file, "w") as writer:
+        logger.info("***** Eval results *****")
+        for key in sorted(eval_output.keys()):
+            writer.write("%s = %s\n" % (key, str(eval_output[key])))
+
+    results.update(eval_output)
+    
+    return results
+
+if __name__ == "__main__":
+    main()
